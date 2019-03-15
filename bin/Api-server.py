ee78927fdf27627d6638b5d5acc6aff56e382f34
luyongjie <luyongjieleo@yahoo.com>
2019-03-15 12:56:06 +0800

    测试功能并入核心代码

--- bin/Api-server.py
+++ bin/Api-server.py
@@ -7,7 +7,7 @@ import sys
 
 BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-from test.IPinit import len_allocat
+from core.IPinit import len_allocat
 
 from core.control import ip, Store
 
