8c02788adfefcc4bd2dc9ad1aba3c37ed2da3958
luyongjie <luyongjie@pconline.com.cn>
2019-08-19 08:42:31 +0800

    更新到新网段+

--- core/IPinit.py
+++ core/IPinit.py
@@ -16,7 +16,7 @@ from requests.auth import HTTPBasicAuth
 class foo:
     @staticmethod
     def get_ip(token):
-        url = "http://192.168.128.201/api/apiclient/addresses/first_free/13/"
+        url = "http://172.18.3.48/api/apiclient/addresses/first_free/13/"
         payload = ""
         headers = {
             'token': token,
@@ -34,7 +34,7 @@ class foo:
     def rec_ip(token,ips):
         if ips =='None':
             return ips
-        url = "http://192.168.128.201/api/apiclient/addresses/" + ips + "/13/"
+        url = "http://172.18.3.48/api/apiclient/addresses/" + ips + "/13/"
         payload = ""
         headers = {
             'token': token,
@@ -47,7 +47,7 @@ class foo:
         print( str(ips) + " dele from ipam")
         return str(ips)
     def token(self):
-        url = "http://192.168.128.201/api/apiclient/user/"
+        url = "http://172.18.3.48/api/apiclient/user/"
         payload = ""
         headers = {}
         try:
@@ -94,7 +94,7 @@ def search_DB():
         'token':IPAM.token() ,
         'cache-control': "no-cache",
     }
-    rev = requests.request("GET", "http://192.168.128.201/api/apiclient/subnets/13/addresses/", headers=headers)
+    rev = requests.request("GET", "http://172.18.3.48/api/apiclient/subnets/13/addresses/", headers=headers)
     dict=[]
     for i in rev.json()['data']:
         if i['tag'] == '2':
